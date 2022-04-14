0db387eb786b6a0f6ae83d3792147f31aeab29de
gioitube <gioitube2k2@gmail.com>
2022-04-14 12:31:29 +0700

    Blog Django v1.0

--- post/forms.py
+++ post/forms.py
@@ -37,41 +37,11 @@ class PostForm(forms.ModelForm):
                 'tags',
                 )
 
-    # def __init__(self, *args, **kwargs):
-    #     super().__init__(*args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-        # for field in self.fields:
-        #     self.fields[field].widget.attrs['class'] = 'form-control'
-
-# class CommentForm(forms.ModelForm):
-#     parent = TreeNodeChoiceField(queryset=Comment.objects.all())
-
-#     content = forms.CharField(widget = CKEditorWidget())
-
-#     def __init__(self, *args, **kwargs):
-#         super().__init__(*args, **kwargs)
-
-#         self.fields['parent'].widget.attrs.update(
-#             {'class': 'd-none'})
-#         self.fields['parent'].label = ''
-#         self.fields['parent'].required = False
-
-#     class Meta:
-#         model = Comment
-#         fields = ('parent', 'content')
-
-#         widgets = {
-#             'content': forms.Textarea(attrs={'class': 'form-control'}),
-#         }
-
-#     def save(self, *args, **kwargs):
-#         Comment.objects.rebuild()
-#         return super(CommentForm, self).save(*args, **kwargs)
-
-
-
-
-
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'form-control'
 
 
 class CommentForm(forms.ModelForm):
