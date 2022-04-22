3eca38d2dc05d913f655e09458a40acc4cb704cc
gioitube <gioitube2k2@gmail.com>
2022-04-22 20:50:09 +0700

    Blog Django v1.0

--- base/settings.py
+++ base/settings.py
@@ -182,18 +182,18 @@ TAGGIT_CASE_INSENSITIVE = True
 
 
 TINYMCE_DEFAULT_CONFIG = {
-    "height": "320px",
+    # "height": "320px",
     # "width": "960px",
-    "menubar": "file edit view insert format tools table help",
-    "plugins": "advlist autolink lists link image charmap print preview anchor searchreplace visualblocks code "
-    "fullscreen insertdatetime media table paste code help wordcount spellchecker",
-    "toolbar": "undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft "
-    "aligncenter alignright alignjustify | outdent indent |  numlist bullist checklist | forecolor "
-    "backcolor casechange permanentpen formatpainter removeformat | pagebreak | charmap emoticons | "
-    "fullscreen  preview save print | insertfile image media pageembed template link anchor codesample | "
-    "a11ycheck ltr rtl | showcomments addcomment code",
-    "custom_undo_redo_levels": 10,
-    "language": "en",  # To force a specific language instead of the Django current language.
+    # "menubar": "file edit view insert format tools table help",
+    # "plugins": "advlist autolink lists link image charmap print preview anchor searchreplace visualblocks code "
+    # "fullscreen insertdatetime media table paste code help wordcount spellchecker",
+    # "toolbar": "undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft "
+    # "aligncenter alignright alignjustify | outdent indent |  numlist bullist checklist | forecolor "
+    # "backcolor casechange permanentpen formatpainter removeformat | pagebreak | charmap emoticons | "
+    # "fullscreen  preview save print | insertfile image media pageembed template link anchor codesample | "
+    # "a11ycheck ltr rtl | showcomments addcomment code",
+    # "custom_undo_redo_levels": 10,
+    # "language": "en",  # To force a specific language instead of the Django current language.
 }
 
 
@@ -302,87 +302,18 @@ TINYMCE_DEFAULT_CONFIG = {
 #     }
 # }
 
-CKEDITOR_CONFIGS = {
-    'default': {
-        'skin': 'moono-lisa',
-        'toolbar_Basic': 'Full',
-        'toolbar_YourCustomToolbarConfig': 'Full',
-        'toolbar': 'Full',  # put selected toolbar config here
-        'toolbarGroups': 'Full',
-        'height': 291,
-        'width': '102%',
-    }
-}
-
-# from ckeditor.configs import DEFAULT_CONFIG  # noqa
-
-# CUSTOM_TOOLBAR = [
-#     {
-#         "name": "document",
-#         "items": [
-#             "Styles",
-#             "Format",
-#             "Bold",
-#             "Italic",
-#             "Underline",
-#             "Strike",
-#             "-",
-#             "TextColor",
-#             "BGColor",
-#             "-",
-#             "JustifyLeft",
-#             "JustifyCenter",
-#             "JustifyRight",
-#             "JustifyBlock",
-#         ],
-#     },
-#     {
-#         "name": "widgets",
-#         "items": [
-#             "Undo",
-#             "Redo",
-#             "-",
-#             "NumberedList",
-#             "BulletedList",
-#             "-",
-#             "Outdent",
-#             "Indent",
-#             "-",
-#             "Link",
-#             "Unlink",
-#             "-",
-#             "Image",
-#             "CodeSnippet",
-#             "Table",
-#             "HorizontalRule",
-#             "Smiley",
-#             "SpecialChar",
-#             "-",
-#             "Blockquote",
-#             "-",
-#             "ShowBlocks",
-#             "Maximize",
-#         ],
-#     },
-# ]
-# CKEDITOR_UPLOAD_PATH = "uploads/"
-# CKEDITOR_IMAGE_BACKEND = "ckeditor_uploader.backends.PillowBackend"
-# CKEDITOR_THUMBNAIL_SIZE = (300, 300)
-# CKEDITOR_IMAGE_QUALITY = 40
-# CKEDITOR_BROWSE_SHOW_DIRS = True
-# CKEDITOR_ALLOW_NONIMAGE_FILES = True
-
 # CKEDITOR_CONFIGS = {
-#     "default": DEFAULT_CONFIG,
-#     "my-custom-toolbar": {
-#         "skin": "moono-lisa",
-#         "toolbar": CUSTOM_TOOLBAR,
-#         "toolbarGroups": None,
-#         "extraPlugins": ",".join(["image2", "codesnippet", "timestamp"]),
-#         "removePlugins": ",".join(["image"]),
-#         "codeSnippet_theme": "xcode",
-#     },
+#     'default': {
+#         'skin': 'moono-lisa',
+#         'toolbar_Basic': 'Full',
+#         'toolbar_YourCustomToolbarConfig': 'Full',
+#         'toolbar': 'Full',  # put selected toolbar config here
+#         'toolbarGroups': 'Full',
+#         'height': 291,
+#         'width': '102%',
+#     }
 # }
+
 
 
 LOGIN_URL = '/accounts/login/'
