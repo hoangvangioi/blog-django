8e998789c1a3cde2c2e1d0a53c4b0c0467094a4e
Hoang Van Gioi <gioitube2k2@gmail.com>
2022-10-09 23:13:46 +0700

    change setting.py

--- base/settings.py
+++ base/settings.py
@@ -45,7 +45,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', default=False)
+DEBUG = False
 
 ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS', default='127.0.0.1')]
 
