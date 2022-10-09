f6fea40bea710851e57adb4fc97d3b6d1e6074b6
Hoang Van Gioi <gioitube2k2@gmail.com>
2022-10-10 00:19:37 +0700

    update templates

--- post/views.py
+++ post/views.py
@@ -70,7 +70,7 @@ def post_detail(request, year, month, day, post):
     # List of similar posts
     post_tags_ids = post.tags.values_list('id', flat=True)
     similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
-    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:5]
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:3]
     current_site = get_current_site(request)
 
     allcomments = post.comments.filter(status=True)
