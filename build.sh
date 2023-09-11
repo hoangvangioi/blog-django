d51b68b199a1fcbfc0d586654c81f98903ef3778
Hoàng Văn Giỏi <gioitube2k2@gmail.com>
2023-09-11 08:59:44 +0700

    add celery to run the task

--- build.sh
+++ build.sh
@@ -7,4 +7,3 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py superuser
-celery -A base beat --scheduler django_celery_beat.schedulers:DatabaseScheduler &
